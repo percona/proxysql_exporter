80746c0227673f2416ea6830699348ed6339b436
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2019-08-07 17:09:29 +0200

    Add more memory metric (#26)
    * Add memory metrics collected from stats_memory_metrics.
    
    This closes https://jira.percona.com/browse/PMM-4348.
    
    * Return rows.Err instead of nil.
    
    * Made new metrics false by default and fix godoc.
    
    * Fix improper merge conflict resolution.

--- proxysql_exporter.go
+++ proxysql_exporter.go
@@ -37,10 +37,11 @@ var (
 	listenAddressF = flag.String("web.listen-address", ":42004", "Address to listen on for web interface and telemetry.")
 	telemetryPathF = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
 
-	mysqlStatusF         = flag.Bool("collect.mysql_status", true, "Collect from stats_mysql_global (SHOW MYSQL STATUS).")
-	mysqlConnectionPoolF = flag.Bool("collect.mysql_connection_pool", true, "Collect from stats_mysql_connection_pool.")
-	mysqlConnectionListF = flag.Bool("collect.mysql_connection_list", true, "Collect connection list from stats_mysql_processlist.")
+	mysqlStatusF                 = flag.Bool("collect.mysql_status", true, "Collect from stats_mysql_global (SHOW MYSQL STATUS).")
+	mysqlConnectionPoolF         = flag.Bool("collect.mysql_connection_pool", true, "Collect from stats_mysql_connection_pool.")
+	mysqlConnectionListF         = flag.Bool("collect.mysql_connection_list", true, "Collect connection list from stats_mysql_processlist.")
 	mysqlDetailedConnectionListF = flag.Bool("collect.detailed.stats_mysql_processlist", false, "Collect detailed connection list from stats_mysql_processlist.")
+	memoryMetricsF               = flag.Bool("collect.stats_memory_metrics", false, "Collect memory metrics from stats_memory_metrics.")
 )
 
 func main() {
@@ -66,7 +67,7 @@ func main() {
 
 	log.Infof("Starting %s %s for %s", program, version.Version, dsn)
 
-	exporter := NewExporter(dsn, *mysqlStatusF, *mysqlConnectionPoolF, *mysqlConnectionListF, *mysqlDetailedConnectionListF)
+	exporter := NewExporter(dsn, *mysqlStatusF, *mysqlConnectionPoolF, *mysqlConnectionListF, *mysqlDetailedConnectionListF, *memoryMetricsF)
 	prometheus.MustRegister(exporter)
 
 	exporter_shared.RunServer("ProxySQL", *listenAddressF, *telemetryPathF, promhttp.ContinueOnError)
